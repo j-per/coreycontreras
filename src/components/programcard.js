cc5ab00a2b1617a9a02768cf286cd646514e7e2d
Jesse Perdue <perdue.jesse@gmail.com>
2019-04-15 20:56:39 -0700

    Changed styles in the program section and created an SVG for the numbers

--- src/components/programcard.js
+++ src/components/programcard.js
@@ -1,16 +1,22 @@
 import React from "react"
 
 import "./bootstrap.min.css"
-import "./programcard.css"
 
 const ProgramCard = props => (
-  <div className="card-wrapper d-flex">
-    <div className="card-number" style={{ backgroundColor: props.color }}>
-      {props.number}
-    </div>
+  <div className="card-wrapper d-flex py-5">
+    <img
+      src={props.circleSVG}
+      alt="test"
+      style={{ width: "60px", alignSelf: "flex-start" }}
+    />
     <div className="card-text-wrapper d-flex flex-column">
-      <div className="card-title">Set Goals</div>
-      <p>
+      <h3
+        className="card-title"
+        style={{ paddingTop: "10px", marginLeft: "5px" }}
+      >
+        {props.title}
+      </h3>
+      <p style={{ marginLeft: "5px" }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
         consectetur massa nunc, ac ornare tellus aliquet rhoncus. Sed pretium,
         risus id consequat mollis, ex enim vestibulum sem, id scelerisque quam
--- src/components/theprogram.js
+++ src/components/theprogram.js
@@ -1,14 +1,48 @@
 import React from "react"
 import ProgramCard from "./programcard"
+import circle1 from "../images/circle-1.svg"
+import circle2 from "../images/circle-2.svg"
+import circle3 from "../images/circle-3.svg"
+import circle4 from "../images/circle-4.svg"
 
-import "./theprogram.css"
+import "./bootstrap.min.css"
 
 const TheProgram = () => (
-  <section className="theprogram-wrapper">
-    <ProgramCard number={1} color={"#FF3B3B"} />
-    <ProgramCard number={2} color={"#0045FF"} />
-    <ProgramCard number={3} color={"#FF7600"} />
-    <ProgramCard number={4} color={"#45D65B"} />
+  <section className="container px-5">
+    <div className="d-flex mx-auto" style={{ flexDirection: "row" }}>
+      <div className="row">
+        <div className="col">
+          <ProgramCard
+            number={1}
+            color={"#FF3B3B"}
+            circleSVG={circle1}
+            title={"Set Goals"}
+          />
+          <ProgramCard
+            number={2}
+            color={"#0045FF"}
+            circleSVG={circle2}
+            title={"Create A Plan Of Action"}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <ProgramCard
+            number={3}
+            color={"#FF7600"}
+            circleSVG={circle3}
+            title={"Meet Weekly For Excercises"}
+          />
+          <ProgramCard
+            number={4}
+            color={"#45D65B"}
+            circleSVG={circle4}
+            title={"Analyze Results"}
+          />
+        </div>
+      </div>
+    </div>
   </section>
 )
 
